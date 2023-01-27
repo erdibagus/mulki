b914b7fc403b0ffa43ec8265134db3741f6de556
Erdi Bagus <erdibgs@gmail.com>
2023-01-27 16:20:30 +0700

    setup server 8

--- config/config.default.php
+++ config/config.default.php
@@ -9,12 +9,12 @@ error_reporting(0);
 $uri = $_SERVER['REQUEST_URI'];
  $pageurl = explode("/", $uri);
  if ($uri == '/') {
- 	$homeurl = "http://" . $_SERVER['HTTP_HOST'];
+ 	$homeurl = "https://" . $_SERVER['HTTP_HOST'];
  	(isset($pageurl[1])) ? $pg = $pageurl[1] : $pg = '';
  	(isset($pageurl[2])) ? $ac = $pageurl[2] : $ac = '';
  	(isset($pageurl[3])) ? $id = $pageurl[3] : $id = 0;
  } else {
-	$homeurl = "http://" . $_SERVER['HTTP_HOST'] . "/" . $pageurl[1];
+	$homeurl = "https://" . $_SERVER['HTTP_HOST'] . "/" . $pageurl[1];
 	(isset($pageurl[2])) ? $pg = $pageurl[2] : $pg = '';
  	(isset($pageurl[3])) ? $ac = $pageurl[3] : $ac = '';
  	(isset($pageurl[4])) ? $id = $pageurl[4] : $id = 0;
